5bebb0927ab908f503084bd1e4d7e13fa057f987
Oleksii <alexmkh89@gmail.com>
2020-05-07 22:06:33 +0300

    Secon half

---
+++ Lesson21-Collections/Program.cs
@@ -0,0 +1,50 @@
+﻿using Lesson21_Collections.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21_Collections
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //var sections = new CustomSections();
+
+            //sections.Add(new Section() { Price = 10, Name = "Valley" });
+            //sections.Add(new Section() { Price = -10, Name = "Footboal" });
+            //sections.Add(new Section() { Price = 100, Name = "Ragby" });
+
+            //Console.WriteLine(sections["Ragby"].Price);
+
+
+            //sections.ForEach(Console.WriteLine);
+            //sections.ForEach(WriteFile);
+
+            var sections = new CustomCollectionFromZero<Section>();
+
+            sections.Add(new Section() { Price = 10, Name = "Valley" });
+            sections.Add(new Section() { Price = -10, Name = "" });
+            sections.Add(new Section() { Price = 100, Name = "Ragby" });
+
+            foreach (var item in sections)
+            {
+                Console.WriteLine(item.Name);   
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static void WriteFile(string data)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter("D:\\test.txt");
+            file.WriteLine(data);
+
+            file.Close();
+        }
+    }
+}
---
+++ Lesson21-Collections/Utils/CustomSections.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21_Collections.Utils
+{
+    public class CustomSections : Collection<Section>
+    {
+        public Section this[string name] => this.Items.First(s => string.Equals(s.Name, name));
+
+        public IEnumerable<string> AllSectionsByName => this.Select(i => i.Name);
+
+        protected override void InsertItem(int index, Section item)
+        {
+            if (item.Price > 0)
+            {
+                base.InsertItem(index, item);
+            }
+            else
+            {
+                Console.WriteLine($"Specified cost isn't valid: {item.Price}");
+            }
+        }
+
+        public void ForEach (Action<string> action)
+        {
+            foreach (var item in Items)
+            {
+                action($"Section name {item.Name} and cost {item.Price}");
+            }
+        }
+    }
+}
