9c8b4d6f37db979d95d806c64e862af3f20e033b
Roman <roma.ne4aev2013@gmail.com>
2020-05-07 20:23:05 +0300

    commit

---
+++ Les21pr/Les21pr/Shop/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les21pr.Shop
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public void GotNewGoods(object sendler, GoodsInfoEventArgs goodInfo)
+        {
+            Console.WriteLine($"The product was recieved: {goodInfo.ProductName}, cost {goodInfo.Cost} by {Name}");
+        }
+        public void GoodsProcessing(object sendler, GoodsInfoEventArgs goodInfo)
+        {
+            Console.WriteLine($"Hi {Name} from second handler!!");
+        }
+
+        public decimal MyDelegateProcessor(decimal i)
+        {
+            return i * i;
+        }
+        //public void ObjectHandler(object sender, object args)
+        //{
+        //    var convertedArgs = args as List<int>;
+        //    foreach(var element in convertedArgs)
+        //    {
+        //        Console.WriteLine(element + "!!");
+        //    }
+        //}
+
+    }
+}
---
+++ Les21pr/Les21pr/Shop/OnlineShop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les21pr.Shop
+{
+    public class OnlineShop
+    {
+        private IList<Product> products = new List<Product>();
+
+        public event EventHandler<GoodsInfoEventArgs> NewProductRecieved = delegate { };
+
+        //public event EventHandler<object> TestObject = delegate { };
+
+        //public delegate decimal MyDelegate(decimal i);
+        //public event MyDelegate TestEvent;
+
+        public void GoodRecieving (Product product)
+        {
+            products.Add(product);
+            NewProductRecieved(null, new GoodsInfoEventArgs() { Cost = product.Cost, ProductName = product.ProductName });
+            //TestEvent?.Invoke(product.Cost);
+
+            //TestObject?.Invoke(null, new List<int>() { 1, 2, 3 });
+        }
+
+    }
+}
