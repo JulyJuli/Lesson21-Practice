6b30c46abed380c8dab62fdedd88372540a9a8fc
Roman <roma.ne4aev2013@gmail.com>
2020-05-07 22:12:34 +0300

    ClassWork+HW17

---
+++ Les21pr/Less21-collection/Units/CustomCollectionFromZero.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less21_collection.Units
+{
+    public class CustomCollectionFromZero<T>: ICollection<T>
+        where T: Section
+    {
+        protected IList<T> Items;
+
+        public CustomCollectionFromZero()
+        {
+            Items = new List<T>();
+        }
+
+        public int Count => Items.Count;
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public T this[int index]=>(T)Items[index];
+        public void Add(T item)
+        {
+            if (!string.IsNullOrEmpty(item.Name))
+            {
+                this.Items.Add(item);
+            }
+        }
+        public void Clear()
+        {
+            Items.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new SectionEnumerator<T>(this);
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Les21pr/Less21-collection/Units/SectionEnumerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less21_collection.Units
+{
+    public class SectionEnumerator<T> : IEnumerator<T>
+        where T : Section
+    {
+
+        public int Counter = -1;
+
+        private readonly CustomCollectionFromZero<T> collection;
+
+        public SectionEnumerator(CustomCollectionFromZero<T> collection)
+        {
+            this.collection = collection;
+        }
+
+
+
+        public T Current => collection[Counter];
+
+        object IEnumerator.Current => collection[Counter];
+
+        public void Dispose()
+        {
+            Counter =-1;
+        }
+
+        public bool MoveNext()
+        {
+            ++Counter;
+            if (collection.Count > Counter)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            Counter = -1;
+        }
+    }
+}
