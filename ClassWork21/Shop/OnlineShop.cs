18a7453ce87df91a5b7eb2cf26151b9683a52036
Oleksii <alexmkh89@gmail.com>
2020-05-07 20:16:30 +0300

    First part

---
+++ ClassWork21/Shop/OnlineShop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork21.Shop
+{
+   public class OnlineShop
+    {
+        private IList<Product> products = new List<Product>();
+        public event EventHandler<GoodsInfoEventsArgs> NewProductReceived = delegate { };
+        public event EventHandler <object>TestObject = delegate { };
+
+        public void GoodRecieving(Product product)
+        {
+            products.Add(product);
+            NewProductReceived(null, new GoodsInfoEventsArgs() { Cost = product.Cost, ProductName = product.ProductName });
+
+            TestObject?.Invoke(null, new List<int>() { 1, 2, 3 });
+        }
+    }
+}
---
+++ ClassWork21/Shop/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork21.Shop
+{
+  public   class Product
+    {
+        public string ProductName { get; set; }
+        public decimal Cost { get; set; }
+        public ProductCategory Category { get; set; }
+    }
+}
