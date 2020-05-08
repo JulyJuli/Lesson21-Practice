3b9439cba34e1e1a5d8674dcd12fb41d662c5319
smokeymoisha <rftg347@gmail.com>
2020-05-08 15:02:58 +0300

    classwork + delegates dictionary

---
+++ Lesson21_Collections/Utils/CustomSections.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21_Collections.Utils
+{
+    public class CustomSections : Collection<Section>
+    {
+        public IEnumerable<string> AllSectionsByName => this.Items.Select(s => s.Name);
+
+        public Section this[string name] => this.Items.First(s => s.Name == name);
+
+        protected override void InsertItem(int index, Section item)
+        {
+            if(item.Cost > 0)
+            {
+                base.InsertItem(index, item);
+            }
+            else
+            {
+                Console.WriteLine($"Specified cost isn't valid: {item.Cost}");
+            }
+        }
+
+        public void ForEach(Action<string> action)
+        {
+            foreach(var item in Items)
+            {
+                action($"Section name {item.Name} and cost {item.Cost}");
+            }
+        }
+    }
+}
---
+++ Lesson21_Collections/Utils/SectionEnumerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21_Collections.Utils
+{
+    public class SectionEnumerator<T> : IEnumerator<T>
+        where T : Section
+    {
+        public int Counter = -1;
+
+        private readonly CustomCollectionFromScratch<T> collection;
+
+        public SectionEnumerator(CustomCollectionFromScratch<T> collection)
+        {
+            this.collection = collection;
+        }
+
+        public T Current => collection[Counter];
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            Counter = -1;
+        }
+
+        public bool MoveNext()
+        {
+            ++Counter;
+            if(collection.Count > Counter)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            Counter = -1;
+        }
+    }
+}
